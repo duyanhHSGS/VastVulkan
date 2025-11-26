0e098576195115779c9ec817dba510e6f8bcc2f8
duyanhHSGS <mrhunter1008@gmail.com>
2025-11-27 00:42:15 +0700

    mesh

--- src/main.cpp
+++ src/main.cpp
@@ -692,7 +692,7 @@ static void create_graphics_pipeline() {
     rasterizer.rasterizerDiscardEnable = VK_FALSE;
     rasterizer.polygonMode = VK_POLYGON_MODE_FILL;
     rasterizer.lineWidth = 1.0f;
-    rasterizer.cullMode = VK_CULL_MODE_BACK_BIT;
+    rasterizer.cullMode = VK_CULL_MODE_NONE;
     rasterizer.frontFace = VK_FRONT_FACE_COUNTER_CLOCKWISE;
     rasterizer.depthBiasEnable = VK_FALSE;
 
